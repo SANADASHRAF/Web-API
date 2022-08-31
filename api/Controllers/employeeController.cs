9f3cb9fd4a9657a196526c21c945c1534e8c8747
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2022-08-31 07:58:00 +0200

    API method

---
+++ api/Controllers/employeeController.cs
@@ -0,0 +1,64 @@
+﻿using api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api.Controllers
+{
+    public class employeeController : ApiController
+    {
+
+        //function to manage your resourse
+        //based convension
+        List<employeecs> lis = new List<employeecs>()
+        {
+            new employeecs(){Id=1,Name="SANAD",salary=30},
+             new employeecs(){Id=2,Name="ali",salary=30},
+              new employeecs(){Id=3,Name="nader",salary=30},
+               new employeecs(){Id=4,Name="samr",salary=30},
+                new employeecs(){Id=5,Name="said",salary=30},
+        };
+
+
+        [HttpGet]
+        public List<employeecs> Get()
+        {
+            return lis;
+        }
+
+        [HttpGet]
+        public employeecs Get(int id)
+        {
+            return lis.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+
+        [HttpPut]
+        public employeecs Put(employeecs em)
+        {
+            lis.Add(em);
+            return em;
+        }
+
+        [HttpPost]
+        public employeecs Post(int id,employeecs em)
+        {
+            var x=lis.Where(n=>n.Id == id).FirstOrDefault();
+            x.Name=em.Name;
+            x.salary = em.salary;
+            return x;
+        }
+
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            var del=lis.Where(x=>x.Id == id).FirstOrDefault();
+            lis.Remove(del);
+            
+        }
+    }
+}
---
+++ api/Models/employeecs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace api.Models
+{
+    public class employeecs
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public int salary { get; set; }
+    }
+}
